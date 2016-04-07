3771ed344d827cbbe8fc606e2dbbb7c07866ebc9
IceMatrix <epicpotatopro@gmail.com>
2016-04-06 22:54:46 -0500

    Listening

--- popup.js
+++ popup.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var enabled;
 
+	var host = "howmanyheyaws.tk";
+
 	var button = document.getElementById("toggleKeyboard");
 	var indicator = document.getElementById("indicator");
 
-	var lowE = new buzz.sound("/audio/voice_e_low_1.wav");
-	var F = new buzz.sound("/audio/voice_f_1.wav");
-	var G = new buzz.sound("/audio/voice_g_1.wav");
-	var A = new buzz.sound("/audio/voice_a_1.wav");
-	var B = new buzz.sound("/audio/voice_b_1.wav");
-	var C = new buzz.sound("/audio/voice_c_1.wav");
-	var D = new buzz.sound("/audio/voice_d_1.wav");
-	var highE = new buzz.sound("/audio/voice_e_high_1.wav");
+	var C = new buzz.sound("/scale/C.wav");
+	var Cs = new buzz.sound("/scale/Cs.wav");
+	var D = new buzz.sound("/scale/D.wav");
+	var Ds = new buzz.sound("/scale/Ds.wav");
+	var E = new buzz.sound("/scale/E.wav");
+	var F = new buzz.sound("/scale/F.wav");
+	var Fs = new buzz.sound("/scale/Fs.wav");
+	var G = new buzz.sound("/scale/G.wav");
+	var Gs = new buzz.sound("/scale/Gs.wav");
+	var hA = new buzz.sound("/scale/hA.wav");
+	var hAs = new buzz.sound("/scale/hAs.wav");
+	var hB = new buzz.sound("/scale/hB.wav");
+	var hC = new buzz.sound("/scale/hC.wav");
+
+	var num_heyaws = 0;
 
 	button.addEventListener("click", function(){
 		if(enabled){
@@ -25,53 +34,109 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	document.addEventListener("keydown", function(event) {
 		if(enabled){
-			//Low E
-			if(event.keyCode == 81){
-				lowE.stop();
-				lowE.play();
-			}
-
-			//F
-			if(event.keyCode == 87){
-				F.stop();
-				F.play();
-			}
-
-			//G
-			if(event.keyCode == 69){
-				G.stop();
-				G.play();
-			}
-
-			//A
-			if(event.keyCode == 82){
-				A.stop();
-				A.play();
-			}
-
-			//B
-			if(event.keyCode == 84){
-				B.stop();
-				B.play();
-			}
-
-			//C
-			if(event.keyCode == 89){
+			if(event.keyCode == 65){
 				C.stop();
 				C.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
 			}
 
-			//D
-			if(event.keyCode == 85){
+			if(event.keyCode == 87){
+				Cs.stop();
+				Cs.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 83){
 				D.stop();
 				D.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
 			}
 
-			//High E
-			if(event.keyCode == 73){
-				highE.stop();
-				highE.play();
+			if(event.keyCode == 69){
+				Ds.stop();
+				Ds.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
 			}
+
+			if(event.keyCode == 68){
+				E.stop();
+				E.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 70){
+				F.stop();
+				F.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 84){
+				Fs.stop();
+				Fs.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 71){
+				G.stop();
+				G.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 89){
+				Gs.stop();
+				Gs.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 72){
+				hA.stop();
+				hA.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 85){
+				hAs.stop();
+				hAs.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 74){
+				hB.stop();
+				hB.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+
+			if(event.keyCode == 75){
+				hC.stop();
+				hC.play();
+				num_heyaws ++;
+				send_heyaw(event.keyCode);
+			}
+		}
+
+		setInterval(incrementHeyaw, 300);
+
+		function incrementHeyaw() {
+			if(num_heyaws > 0) {
+				$.get("http://" + host + ":4433/add?num=" + num_heyaws, function(data){});
+				num_heyaws = 0;
+			}
+		}
+
+		function send_heyaw(code) {
+			$.get("http://" + host + ":4433/play?code=" + code, function(data){});
 		}
 	})
 }, false);
